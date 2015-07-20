f8797115f32f46e351a93c342442f55055a04a73
Kelly Galway <kgalway@hotmail.com>
2015-07-20 08:28:37 -0700

    Forgot to add to last commit

--- ical_writer.py
+++ ical_writer.py
@@ -9,71 +9,18 @@ import pandas as pd
 from datetime import timedelta, time, datetime  
 import pytz 
 
+
+from people import people, emails
+from calendar_utils import copy_current_to_archive, cancel_calendar, cancel_event, person_calendar
+
+
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 handler.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 
-def copy_current_to_archive():
-    archive = os.path.join('.', 'archive')
-    current = os.path.join('.','current')
-    if os.listdir(current):
-    # if there is actualy anything in the current folder 
-        for fil in os.listdir(current):
-            archive_path = os.path.join(archive, fil)
-            current_path = os.path.join(current,fil)
-            shutil.copyfile(current_path, archive_path)
-            logger.info('copied {0} to {1}'.format(current_path, archive_path))
 
-def cancel_event(event):
-    event.status = 'CANCELLED'
-    event.method = 'CANCEL'
-    event.sequence = 2
-    return event 
-
-def cancel_calendar(calendar):
-    new_cal = ics.Calendar()
-    for event in calendar.events:
-        if event.status == 'CANCELLED' or event.begin < datetime.today().replace(tzinfo = pacific):
-            # found an already-cancelled event. Skip it 
-            logger.debug('Ignoring event {0}'.format(str(event)))
-            continue
-        event = cancel_event(event)
-        new_cal.events.append(event)
-    return new_cal         
-
-def person_calendar(schedule):
-    """ create a list of events for a person based on a tuple of (date, shift) tuples """
-    cal = ics.Calendar() 
-      
-    for date, shift in schedule:
-        if date < today or shift == 'ignore':
-            continue
-        event = ics.Event()
-        event.name = shift 
-        if shift in shift_map:
-            shift_startTime, shift_length = shift_map[shift]
-        else:
-            shift_startTime, shift_length  = shift_map['other']
-        
-        start_time = date + timedelta(hours = shift_startTime.hour, minutes = shift_startTime.minute)
-        event.begin = start_time 
-        if shift_length is not None:
-            event.duration = shift_length
-        else:
-            event.make_all_day()
-
-        cal.events.append(event)
-    return cal
-
-def write_ical(person, calendar):
-    dirpath = 'current'
-    filename = person + '.ics'
-    filepath = os.path.join(dirpath,filename)
-    with open(filepath,'w') as f:
-        f.writelines(calendar)
-    f.close()
 
 
 
